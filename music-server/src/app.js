4c4d2e6a1d33a6b52e9582614a268932d3f7d504
Suresh Bamma <sureshbamma321@gmail.com>
2024-04-20 00:01:12 -0500

    playlist page

--- music-server/src/app.js
+++ music-server/src/app.js
@@ -20,6 +20,6 @@ app.use((req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('server listening on 3000');
-})
+app.listen(8000, () => {
+    console.log('server listening on 8000');
+});
